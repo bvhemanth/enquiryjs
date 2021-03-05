3b95359bbd3b4775f015bfe9b750723e3d55d66e
HemanthNewage <hemanthkumar.bv@newage-global.com>
2021-03-06 01:26:33 +0530

    feat(test): unittest cases

--- src/app/common/pagination/pagination.component.spec.ts
+++ src/app/common/pagination/pagination.component.spec.ts
@@ -2,18 +2,19 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PaginationComponent } from './pagination.component';
 import { TodoAction } from 'src/app/stores/todo/todo.actions';
-import { Store, NgxsModule } from '@ngxs/store';
+import { Action, NgxsModule, State, StateContext, Store, Selector } from '@ngxs/store';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
+import * as action from 'src/app/stores/todo/todo.actions';
 
-describe('PaginationComponent', () => {
+fdescribe('PaginationComponent', () => {
   let component: PaginationComponent;
   let fixture: ComponentFixture<PaginationComponent>;
-
+  let store;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ PaginationComponent ],
       imports:[
-        NgxsModule.forRoot([TodoAction])
+        NgxsModule.forRoot([TodoState])
       ],
       schemas: [NO_ERRORS_SCHEMA],
     })
@@ -22,6 +23,7 @@ describe('PaginationComponent', () => {
 
   beforeEach(() => {
     fixture = TestBed.createComponent(PaginationComponent);
+    store = TestBed.inject(Store);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
@@ -30,3 +32,32 @@ describe('PaginationComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+
+
+export interface TodoStateModel {
+  //Stores
+}
+
+@State<TodoStateModel>({
+  name: "Todo",
+  defaults: {
+    Stores:{
+      searchText:null,
+    },
+  }
+})
+export class TodoState {
+
+  @Selector()
+  public static getState(state: TodoStateModel) {
+    return state;
+  }
+
+  @Action(action.TodoAction)
+  public add(ctx: StateContext<TodoStateModel>, actions: action.TodoAction) {
+    const stateModel = ctx.getState();
+    console.log(actions.payload)
+    //stateModel.stores = [...stateModel.stores, payload];
+    ctx.patchState({ ...stateModel, Stores:actions.payload});
+  }
+}
